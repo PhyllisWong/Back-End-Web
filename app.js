fc8fa46d1c07adeaf95d18dee4504238065a5ea6
Phyllis Wong <wong.phyllism@gmail.com>
2018-09-04 15:14:08 -0700

    add folder structure

--- app.js
+++ app.js
@@ -6,32 +6,56 @@ const port = process.env.PORT || '3000';
 module.exports = (app) => {
   // Data JSON object
   const castData = {
-    title: 'Under The Dress',
-    cast: {
-      Ellis: 'Marlyn Monroe',
-      Jared: 'Brad Pitt',
-      Eric: 'Matt Damon'
+    {
+      id: 1,
+      title: 'Under The Dress',
+      cast: {
+        Ellis: 'Marlyn Monroe',
+        Jared: 'Brad Pitt',
+        Eric: 'Matt Damon'
+      }
     },
-    title: 'Game of Thrones',
-    cast: {
-      Targaeryan: 'Dani',
-      Snow: 'Joe',
-      Stark: 'Ansel',
-      Lanister: 'Kendra'
+
+    {
+      id: 2,
+      title: 'Game of Thrones',
+      cast: {
+        Targaeryan: 'Dani',
+        Snow: 'Joe',
+        Stark: 'Ansel',
+        Lanister: 'Kendra'
+      }
     },
-    title: 'Breaking Bad',
-    cast: {
-      Jessie: 'Aaron Paul',
-      White: 'Brian Cramson',
-      Saul: 'Bob Odenkirk'
+
+    {
+      id: 3,
+      title: 'Breaking Bad',
+      cast: {
+        Jessie: 'Aaron Paul',
+        White: 'Brian Cramson',
+        Saul: 'Bob Odenkirk'
+      }
     }
-  }
+
+  };
   // Routes
   // Root route
-  app.get('/cast/:title', (req, res) => {
-    let title = req.params.title;
-    res.json(castData.title)
+  app.get('/', (req, res) => {
+    // let id = req.params.id;
+    res.json(castData)
   });
+
+
+
+
+  app.get('/cast/:id', (req, res) => {
+    // console.log(castData.one);
+    let id = req.params.id;
+    // console.log(castData.one)
+
+    console.log(castData.id)
+    res.json(castData.id)
+  })
 }
 
 // app.listen(port, () => {
